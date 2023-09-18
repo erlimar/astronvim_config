7ae20e2b4283668fb084704a2f0d667d39e226f2
Erlimar Silva Campos <erlimar@gmail.com>
2023-09-18 17:34:21 -0300

    Ajusta configuração para multiplataforma

--- options.lua
+++ options.lua
@@ -1,30 +1,29 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
-return {
-  opt = {
-    -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
-    shell = "pwsh",
-    shellcmdflag = "-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-  },
-  g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
-  },
-}
+return function(local_vim)
+  local_vim.opt.relativenumber = true -- sets vim.opt.relativenumber
+  local_vim.opt.number = true -- sets vim.opt.number
+  local_vim.opt.spell = false -- sets vim.opt.spell
+  local_vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+  local_vim.opt.wrap = false -- sets vim.opt.wrap
+  if vim.fn.executable('pwsh') == 1 then
+    local_vim.opt.shell = "pwsh"
+    local_vim.opt.shellcmdflag = "-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    local_vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    local_vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    local_vim.opt.shellquote = ""
+    local_vim.opt.shellxquote = ""
+  end
+
+  local_vim.g.mapleader = " " -- sets vim.g.mapleader
+  local_vim.g.autoformat_enabled = true -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+  local_vim.g.cmp_enabled = true -- enable completion at start
+  local_vim.g.autopairs_enabled = true -- enable autopairs at start
+  local_vim.g.diagnostics_mode = 3 -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+  local_vim.g.icons_enabled = true -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+  local_vim.g.ui_notifications_enabled = true -- disable notifications when toggling UI elements
+  local_vim.g.resession_enabled = false -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+  return local_vim
+end
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
 --   local_vim.opt.relativenumber = true
